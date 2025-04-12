c2f167c6f00667d9abd7117e05d0075524240c33
sandramarincodes <smarinfernan@gmail.com>
2025-04-12 14:44:43 +0200

    Update ejercicio 4

---
+++ ejercicio 4/js/main.js
@@ -0,0 +1,16 @@
+
+const input = document.querySelector(".js_input");
+const parrafo = document.querySelector(".js_parrafo");
+
+
+input.addEventListener("click", () => {
+   console.log("Ha hecho click");
+   parrafo.innerHTML =
+
+/* const boton = document.querySelector(".js_btn");
+
+
+boton.addEventListener("click", (ev) => {
+    console.log("ha hecho click");
+    boton.classList.toggle("btn");
+}) */
