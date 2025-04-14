04cc41928861ee5725069e707b6e57099f617088
Paula-51 <paulasanjosemartin@gmail.com>
2025-04-14 21:43:20 +0200

    ejercicios javascript 14-04

---
+++ ejercicios condicionales/js/main.js
@@ -0,0 +1,53 @@
+// const input = document.querySelector(".js_input");
+// const texto = document.querySelector(".js-text");
+
+// nombre1 = "Maria";
+// input.addEventListener("input", (ev)=> {
+//     if (input.value===nombre1
+//     || input.value==="Luisa") {
+//     texto.innerHTML="Bienvenida, " +input.value}
+//     else {texto.innerHTML="Lo siento pero el usuario que has introducido no está registrado"}
+// })
+
+/* const number = 123123125;
+
+if (number ===0) {
+    console.log('El número es 0')
+  } else if (number <0) {
+    console.log('El número es negativo')
+
+  } else if (number+ 2 > 13 && number+ 2 <= 20) {
+    console.log('El número más 2 es mayor que 13 pero menor o igual que 20')
+
+  } else if (number >20 && number <50) {
+    console.log('El número es mayor que 20 pero menor que 50')
+  } else {
+    console.log('el número no es 123123125')
+  } */
+/* 
+    "use strict";
+
+// avatar por defecto
+const DEFAULT_AVATAR = "http://placehold.jp/150x150.png";
+// avatar que eligió el usuario al registrarse
+let userAvatar = "https://placecats.com/300/200";
+
+const img = document.querySelector(".js-user__avatar");
+img.src = userAvatar;
+
+if (userAvatar==="") {
+    img.src=DEFAULT_AVATAR;
+} */
+
+    const perro = 3;
+    let humano; // Se declara fuera de los bloques
+    
+    if (perro === 1) {
+        humano = perro * 15;
+    } else if (perro === 2) {
+        humano = perro * 9;
+    } else if (perro >= 3) {
+        humano = perro * 5;
+    }
+    
+    console.log(humano);
