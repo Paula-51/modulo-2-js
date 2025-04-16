213cfca25c075d7b46309c9798344ceafb2342e3
Paula-51 <paulasanjosemartin@gmail.com>
2025-04-16 22:04:08 +0200

    ejercicios 1.04

---
+++ ejercicios-funciones/main.js
@@ -0,0 +1,51 @@
+/* function multiplicacion(numero1, numero2){
+    let producto= numero1*numero2;
+    return producto;
+}
+
+let resultado= multiplicacion (11,7);
+console.log(resultado)
+
+function media (nota1, nota2, nota3,nota4){
+    let notaSuma= nota1+nota2+nota3+nota4;
+    let notaMedia= notaSuma/4;
+    return notaMedia;
+}
+console.log (media(5,5,4,5));
+
+function numeroPar (n){
+    if (n %2 ===0){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log(numeroPar(7)); */
+
+/* function precio (n){
+    let precioIva= (n*0.21)+n;
+    return `precio sin Iva: ${n},Iva: 2.1 y total: 
+    ${precioIva}`;
+}
+console.log(precio(10)); */
+// modificamos una variable de ámbito global
+/* let secretLetter = 'y';
+function mySecretLetter() {
+  secretLetter = 'x';
+  return secretLetter;
+}
+console.log(secretLetter); // devuelve "x"
+console.log(mySecretLetter()); // devuelve "x" */
+
+/* function getEl (clase){
+    let btn =document.querySelector(clase);
+    return btn;
+   }
+
+const ul= getEl(".js-ul");
+console.log (ul); */
+
+
+
+
