d47811db02f5607d4f1cbffa294743b97b4cf6ca
Paula-51 <paulasanjosemartin@gmail.com>
2025-04-26 10:40:40 +0200

    array

---
+++ ejercicios array/js/main.js
@@ -0,0 +1,16 @@
+//Ejercicios funciones//
+
+function workWithMovies (){
+    const peliculas = document.querySelector(".js-movies");
+    const listaPelis=[
+    "Titanic", "Shutter Island","Origen"
+    ]
+    
+    listaPelis[3]= "La Sirenita";
+    listaPelis[2]= "La Sirenita2";
+    console.log (listaPelis);
+}
+
+workWithMovies ();
+
+
