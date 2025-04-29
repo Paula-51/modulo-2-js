2547a58b68c10250271f672caf3d82fb1023e1ef
Paula-51 <paulasanjosemartin@gmail.com>
2025-04-29 21:49:17 +0200

    ejercicios bucles y array

---
+++ ejercicios-arrays-bucles/js/main.js
@@ -0,0 +1,17 @@
+/* function get100numbers (){
+    const numbers = []
+    for (let i=1; i<101;i++){
+        numbers.push (i);
+    }
+    return numbers;
+}
+
+const numeros = get100numbers ();
+numeros.forEach((element) => {console.log(element);})
+
+function getReversed100numbers (){
+    const numIvertidos = get100numbers ();
+    return numIvertidos.reverse ();
+}
+const numerosInvertidos = getReversed100numbers ();
+numerosInvertidos.forEach((element) => {console.log(element);}) */
